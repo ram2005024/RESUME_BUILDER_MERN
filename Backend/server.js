789b14d33dbf73592829614ecf083c65f8638e35
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-26 17:14:30 +0545

    Initial Commit

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,10 @@ if (prisma) {
 //---------------------Middlewares------------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://frotend-resume.onrender.com/"
+        : "http://localhost:5173",
     credentials: true,
   })
 );
