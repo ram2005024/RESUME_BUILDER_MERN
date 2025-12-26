ff01e2dc3e9d21483cb8f67b4fa394ad18b4b542
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-26 20:56:06 +0545

    Final

--- Backend/config/multer.js
+++ Backend/config/multer.js
@@ -1,3 +1,12 @@
 import multer from "multer";
 
-export const upload = multer({ storage: multer.memoryStorage() });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads"); // create 'uploads' folder in your project
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+export const upload = multer({ storage });
