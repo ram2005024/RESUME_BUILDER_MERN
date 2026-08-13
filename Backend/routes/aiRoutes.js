acaab7a2677b42ead1851702d4779909538ad837
ram2005024 <sharmashekhar20050@gmail.com>
2026-08-13 17:10:52 +0545

    added pdf parse package

--- Backend/routes/aiRoutes.js
+++ Backend/routes/aiRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { protectUserAuth } from "../middlewares/protect.js";
 import { enhanceText, generateResume } from "../controllers/aiController.js";
+import { upload } from "./../config/multer";
 export const aiRoute = express.Router();
 aiRoute.post("/enhanceText", protectUserAuth, enhanceText);
-aiRoute.post("/generateResume", protectUserAuth, generateResume);
+aiRoute.post(
+  "/generateResume",
+  protectUserAuth,
+  upload.single("file"),
+  generateResume,
+);
